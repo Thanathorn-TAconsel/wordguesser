16fa6ba4447e89713b1b45893478b207fe1fe558
TAconsel <taconsel123@gmail.com>
2022-08-27 19:12:09 +0700

    Part 1 is Complete

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -7,8 +7,72 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @guesses = '' 
+    @wrong_guesses = ''
+    @ary = Array.new
+    @timeguess = 0
   end
 
+  def word_with_guesses()
+    output = ""
+    @word.split('').each { |c|
+      if @ary.include? c
+        output += c
+      else
+        output += '-'
+      end
+    }
+    return output
+  end
+
+  def check_win_or_lose()
+    if @timeguess >= 7
+      return :lose
+    end
+    @word.split('').each { |c|
+      if @ary.include? c
+        
+      else
+        return :play
+      end
+    }
+    return :win
+  end
+
+  def guess(uin)
+    if uin == nil
+      raise ArgumentError, 'Argument Error'
+    end
+    if uin.length == 0
+      raise ArgumentError, 'Argument Error'
+    end
+    uin = uin.downcase
+    if uin.match(/[^a-zA-Z]/)
+      raise ArgumentError, 'Argument Error'
+    end
+    if @ary.include? uin
+      return false
+    end
+    if @word.include? uin
+      @guesses = uin
+    else
+      @wrong_guesses = uin
+    end
+    @ary.append(uin)
+    @timeguess += 1
+  end
+
+  def word
+    @word
+  end
+
+  def guesses
+    @guesses
+  end
+
+  def wrong_guesses
+    @wrong_guesses
+  end
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
